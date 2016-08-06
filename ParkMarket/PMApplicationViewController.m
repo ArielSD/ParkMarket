350f4d1ddf7f13f1fd7599a2261ede60107b6cfe
ArielSD <ascottdicker@dosomething.org>
2016-08-06 19:27:02 -0400

    Refactor container view controller code into helper methods

--- ParkMarket/PMApplicationViewController.m
+++ ParkMarket/PMApplicationViewController.m
@@ -18,32 +18,42 @@
     [super viewDidLoad];
     
     // Comment out to keep a user signed in
-//    NSError *error;
-//    [[FIRAuth auth] signOut:&error];
-//    if (!error) {
-//        NSLog(@"A user has been signed out");
-//    }
+    NSError *error;
+    [[FIRAuth auth] signOut:&error];
+    if (!error) {
+        NSLog(@"A user has been signed out");
+    }
     
     if ([FIRAuth auth].currentUser) {
-        PMInitialViewController *initialViewController = [PMInitialViewController new];
-        UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:initialViewController];
-        [self addChildViewController:navigationController];
-        navigationController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-        [self.view addSubview:navigationController.view];
-        [navigationController didMoveToParentViewController:self];
+        [self showInitialViewController];
     }
     else {
-        PMLoginViewController *loginViewController = [PMLoginViewController new];
-        [self addChildViewController:loginViewController];
-        loginViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-        [self.view addSubview:loginViewController.view];
-        [loginViewController didMoveToParentViewController:self];
+        [self showLoginViewController];
     }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     NSLog(@"Did receive memory warning");
+}
+
+#pragma - Helper Methods
+
+- (void)showLoginViewController {
+    PMLoginViewController *loginViewController = [PMLoginViewController new];
+    [self addChildViewController:loginViewController];
+    loginViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view addSubview:loginViewController.view];
+    [loginViewController didMoveToParentViewController:self];
+}
+
+- (void)showInitialViewController {
+    PMInitialViewController *initialViewController = [PMInitialViewController new];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:initialViewController];
+    [self addChildViewController:navigationController];
+    navigationController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view addSubview:navigationController.view];
+    [navigationController didMoveToParentViewController:self];
 }
 
 @end
