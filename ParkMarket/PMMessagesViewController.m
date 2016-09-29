2b6c16332aa46bb9ce1a4d7ed9a0197fbe8f867b
ArielSD <ascottdicker@dosomething.org>
2016-09-29 12:06:27 -0400

    Added test 'dismiss' button to messages view controller

--- ParkMarket/PMMessagesViewController.m
+++ ParkMarket/PMMessagesViewController.m
@@ -10,17 +10,46 @@
 
 @interface PMMessagesViewController ()
 
+// Testing
+@property(strong, nonatomic) UIButton *dismissButton;
+// Testing
+
 @end
 
 @implementation PMMessagesViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Testing
+    [self configureDismissButton];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     NSLog(@"Did receive memory warning");
+}
+
+// Testing
+- (void)configureDismissButton {
+    self.dismissButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.view addSubview:self.dismissButton];
+    
+    self.dismissButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.dismissButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [self.dismissButton.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+    
+    [self.dismissButton setTitle:@"Dismiss"
+                        forState:UIControlStateNormal];
+    
+    [self.dismissButton addTarget:self
+                           action:@selector(dismissButtonTapped)
+                 forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)dismissButtonTapped {
+    [self dismissViewControllerAnimated:YES
+                             completion:nil];
 }
 
 @end
